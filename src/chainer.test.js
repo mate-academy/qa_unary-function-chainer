6c14a9c66a93c4a3c405335ab0c444f7a7b06344
Vlad <bojko.vladislav16@gmail.com>
2023-06-10 15:50:49 +0300

    Some changes were made

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { chainer } = require('./chainer');
 
 describe('chainer', () => {
@@ -11,13 +13,15 @@ describe('chainer', () => {
 
   it('should return the initial value when no functions are provided', () => {
     const result = chainer([])(5);
+
     expect(result).toBe(5);
   });
 
-  it('should return the input value unchanged when a single identity function is provided', () => {
+  it(`should return the input value unchanged 
+    when a single identity function is provided`, () => {
     const identity = jest.fn(x => x);
     const result = chainer([identity])(10);
-    
+
     expect(result).toBe(10);
     expect(identity).toHaveBeenCalledWith(10);
   });
@@ -34,5 +38,4 @@ describe('chainer', () => {
     expect(square).toHaveBeenCalledWith(6);
     expect(increment).toHaveBeenCalledWith(36);
   });
-})
-
+});
