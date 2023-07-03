3c9c0552d4f4b20517066b6e1ae7caf65f12e363
Viktoriia Tykhomyrova <viktoriavintonak0@gmail.com>
2023-07-03 21:40:05 +0200

    chainer

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,1 +1,31 @@
+'use strict';
+
 const { chainer } = require('./chainer');
+
+describe('chainer', () => {
+  it('should chain the unary functions correctly', () => {
+    const mockFunctionA = jest.fn((x) => x + 1);
+    const mockFunctionB = jest.fn((x) => x * 2);
+    const mockFunctionC = jest.fn((x) => x - 3);
+    const mockFunctionD = jest.fn((x) => x / 4);
+
+    const chain = chainer([mockFunctionA, mockFunctionB,
+      mockFunctionC, mockFunctionD]);
+
+    const input = 10;
+    const result = chain(input);
+
+    expect(mockFunctionA).toHaveBeenCalledWith(input);
+    expect(mockFunctionB).toHaveBeenCalledWith(mockFunctionA(input));
+
+    expect(mockFunctionC)
+      .toHaveBeenCalledWith(mockFunctionB(mockFunctionA(input)));
+
+    expect(mockFunctionD)
+      .toHaveBeenCalledWith(mockFunctionC(mockFunctionB(mockFunctionA(input))));
+
+    expect(result)
+      .toEqual(mockFunctionD(mockFunctionC(mockFunctionB(mockFunctionA(input)))
+      ));
+  });
+});
