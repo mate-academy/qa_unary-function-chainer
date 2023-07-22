8f773737b2375b944bb58dd73aff28d777a0e2cc
Dmytro Budynskyi <dbudinskyy@gmail.com>
2023-07-22 15:53:16 +0200

    Solution 22072023

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -2,47 +2,31 @@
 
 describe('chainer', () => {
   const { chainer } = require('./chainer');
+  const addOne = (x) => x + 1;
+  const multiplyByTwo = (x) => x * 2;
+  const subtractThree = (x) => x - 3;
+
+  const chainedFunctions = chainer([addOne, multiplyByTwo, subtractThree]);
+
+  it('should be declared', () => {
+    expect(chainer).toBeInstanceOf(Function);
+  });
+
+  it('should return function', () => {
+    expect(chainer()).toBeInstanceOf(Function);
+  });
 
   it(`should correctly chain functions and return the expected result`, () => {
-    const addOne = (x) => x + 1;
-    const multiplyByTwo = (x) => x * 2;
-    const subtractThree = (x) => x - 3;
-
-    const chainedFunctions = chainer([addOne, multiplyByTwo, subtractThree]);
-
     expect(chainedFunctions(5)).toEqual(9);
   });
 
   it(`should correctly chain functions and return the expected'
-  + 'result for 0 value`, () => {
-    const addOne = (x) => x + 1;
-    const multiplyByTwo = (x) => x * 2;
-    const subtractThree = (x) => x - 3;
-
-    const chainedFunctions = chainer([addOne, multiplyByTwo, subtractThree]);
-
+  + 'result for null value`, () => {
     expect(chainedFunctions(0)).toEqual(-1);
   });
 
-  it(`should correctly chain functions and return the expected'
-  + 'result for null value`, () => {
-    const addOne = (x) => x + 1;
-    const multiplyByTwo = (x) => x * 2;
-    const subtractThree = (x) => x - 3;
-
-    const chainedFunctions = chainer([addOne, multiplyByTwo, subtractThree]);
-
-    expect(chainedFunctions(null)).toEqual(-1);
-  });
-
-  it(`should correctly chain functions and return the expected'
-  + 'result for undefined value`, () => {
-    const addOne = (x) => x + 1;
-    const multiplyByTwo = (x) => x * 2;
-    const subtractThree = (x) => x - 3;
-
-    const chainedFunctions = chainer([addOne, multiplyByTwo, subtractThree]);
-
+  it(`should correctly chain functions and return 
+    the expected result for undefined value`, () => {
     const result = chainedFunctions(undefined);
 
     expect(Number.isNaN(result)).toBe(true);
