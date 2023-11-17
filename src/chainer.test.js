c81984d2b542a63e07ca450b55a3cb42a3f1f295
Illia Vinkov <illia.vinkov@gmail.com>
2023-11-17 23:23:37 +0100

    solution

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,1 +1,25 @@
-const { chainer } = require('./chainer');
+'use strict';
+
+describe('chainer', () => {
+  const { chainer } = require('./chainer');
+
+  it('should return function', () => {
+    const result = chainer([]);
+
+    expect(typeof result).toBe('function');
+  });
+
+  it('that called every func and return result', () => {
+    const f1 = jest.fn((x) => x * 2);
+    const f2 = jest.fn((x) => x + 2);
+    const f3 = jest.fn((x) => Math.pow(x, 2));
+
+    const func = chainer([f1, f2, f3]);
+    const result = func(0);
+
+    expect(f1).toHaveBeenCalled();
+    expect(f2).toHaveBeenCalled();
+    expect(f3).toHaveBeenCalled();
+    expect(result).toBe(4);
+  });
+});
