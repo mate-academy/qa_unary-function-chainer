762945d4841449c1e2827fb3b5ac4289535a1871
Nazar <nazarorlov552@gmail.com>
2024-01-02 11:06:17 +0200

    test2

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,30 +1,43 @@
-const { chainer } = require('./chainer');
 'use strict';
 
-  it(`should be declared`, () => {
-    expect(chainer).toBeInstanceOf(Function);
-  });
+const { chainer } = require('./chainer');
 
-  it('should return a function', () => {
-    const firstFunction = jest.fn();
-    const secondFunction = jest.fn();
-    const result = chainer([firstFunction, secondFunction]);
+it(`should be declared`, () => {
+  expect(chainer).toBeInstanceOf(Function);
+});
 
-    expect(result).toBeInstanceOf(Function);
-  });
+it('should return a function', () => {
+  const firstFunction = jest.fn();
+  const secondFunction = jest.fn();
+  const result = chainer([firstFunction, secondFunction]);
 
-  it('should chain functions and return final result', () => {
-    const mock1 = jest.fn((x) => x * 2);
-    const mock2 = jest.fn((x) => x + 2);
-    const mock3 = jest.fn((x) => Math.pow(x, 2));
+  expect(result).toBeInstanceOf(Function);
+});
 
-    const chainedFunction = chainer([mock1, mock2, mock3]);
-    const result = chainedFunction(0);
+it('should chain functions with 0 return final result 4', () => {
+  const mock1 = jest.fn((x) => x * 2);
+  const mock2 = jest.fn((x) => x + 2);
+  const mock3 = jest.fn((x) => Math.pow(x, 2));
 
-    expect(mock1).toHaveBeenCalledWith(0);
-    expect(mock2).toHaveBeenCalledWith(0);
-    expect(mock3).toHaveBeenCalledWith(2);
-    expect(result).toBe(4);
-  });
+  const chainedFunction = chainer([mock1, mock2, mock3]);
+  const result = chainedFunction(0);
 
+  expect(mock1).toHaveBeenCalledWith(0);
+  expect(mock2).toHaveBeenCalledWith(0);
+  expect(mock3).toHaveBeenCalledWith(2);
+  expect(result).toBe(4);
+});
 
+it('should chain functions 1 return final result 16', () => {
+  const mock1 = jest.fn((x) => x * 2);
+  const mock2 = jest.fn((x) => x + 2);
+  const mock3 = jest.fn((x) => Math.pow(x, 2));
+
+  const chainedFunction = chainer([mock1, mock2, mock3]);
+  const result = chainedFunction(1);
+
+  expect(mock1).toHaveBeenCalledWith(1);
+  expect(mock2).toHaveBeenCalledWith(2);
+  expect(mock3).toHaveBeenCalledWith(4);
+  expect(result).toBe(16);
+});
