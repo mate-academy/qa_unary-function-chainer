a6e9a3fd50aabe56281f00677a363bea39c14091
Bogdan Stratiychuk <stratiychuk.bogdan@gmail.com>
2024-04-12 21:32:24 +0200

    Solution

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,59 +1,60 @@
+'use strict';
+
 const { chainer } = require('./chainer');
 
 describe('Chainer', () => {
-    
-    it('should apply each function from array to the input', () => {
-        const addOne = (x) => x + 1;
-        const double = (x) => x * 2;
-        const square = (x) => x * x;
+  it('should apply each function from array to the input', () => {
+    const addOne = (x) => x + 1;
+    const double = (x) => x * 2;
+    const square = (x) => x * x;
 
-        const funcsArray = [addOne, double, square];
-        const chainedFunction = chainer(funcsArray);
+    const funcsArray = [addOne, double, square];
+    const chainedFunction = chainer(funcsArray);
 
-        expect(chainedFunction(2)).toBe(36);
-    });
+    expect(chainedFunction(2)).toBe(36);
+  });
 
-    it('should handle negative input', () => {
-        const addOne = (x) => x + 1;
-        const double = (x) => x * 2;
-        const square = (x) => x * x;
+  it('should handle negative input', () => {
+    const addOne = (x) => x + 1;
+    const double = (x) => x * 2;
+    const square = (x) => x * x;
 
-        const funcsArray = [addOne, double, square];
-        const chainedFunction = chainer(funcsArray);
+    const funcsArray = [addOne, double, square];
+    const chainedFunction = chainer(funcsArray);
 
-        expect(chainedFunction(-3)).toBe(16);
-    });
+    expect(chainedFunction(-3)).toBe(16);
+  });
 
-    it('should handle "undefined" input', () => {
-        const addOne = (x) => x + 1;
-        const double = (x) => x * 2;
-        const square = (x) => x * x;
+  it('should handle "undefined" input', () => {
+    const addOne = (x) => x + 1;
+    const double = (x) => x * 2;
+    const square = (x) => x * x;
 
-        const funcsArray = [addOne, double, square];
-        const chainedFunction = chainer(funcsArray);
+    const funcsArray = [addOne, double, square];
+    const chainedFunction = chainer(funcsArray);
 
-        expect(chainedFunction(undefined)).toBe(NaN);
-    });
+    expect(chainedFunction(undefined)).toBe(NaN);
+  });
 
-    it('should handle "null" input', () => {
-        const addOne = (x) => x + 1;
-        const double = (x) => x * 2;
-        const square = (x) => x * x;
+  it('should handle "null" input', () => {
+    const addOne = (x) => x + 1;
+    const double = (x) => x * 2;
+    const square = (x) => x * x;
 
-        const funcsArray = [addOne, double, square];
-        const chainedFunction = chainer(funcsArray);
+    const funcsArray = [addOne, double, square];
+    const chainedFunction = chainer(funcsArray);
 
-        expect(chainedFunction(null)).toBe(4);
-    });
+    expect(chainedFunction(null)).toBe(4);
+  });
 
-    it('should return "Nan" when runned without input', () => {
-        const addOne = (x) => x + 1;
-        const double = (x) => x * 2;
-        const square = (x) => x * x;
+  it('should return "Nan" when runned without input', () => {
+    const addOne = (x) => x + 1;
+    const double = (x) => x * 2;
+    const square = (x) => x * x;
 
-        const funcsArray = [addOne, double, square];
-        const chainedFunction = chainer(funcsArray);
+    const funcsArray = [addOne, double, square];
+    const chainedFunction = chainer(funcsArray);
 
-        expect(chainedFunction()).toBe(NaN);
-    });
+    expect(chainedFunction()).toBe(NaN);
+  });
 });
