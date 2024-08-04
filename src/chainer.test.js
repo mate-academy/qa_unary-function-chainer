54618a3c6960fa4d59fa2a3298543d3bf7232506
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-08-04 20:45:18 +0300

    qweqwe

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -9,14 +9,15 @@ describe('chainer', () => {
     expect(result).toBeInstanceOf(Function);
   });
 
-  it('should chain all functions', () => {
+  it('should chain all functions correctly', () => {
     const mockAddOne = jest.fn(x => x + 1);
     const mockDouble = jest.fn(x => x * 2);
     const mockSquare = jest.fn(x => x * x);
 
     const functions = [mockAddOne, mockDouble, mockSquare];
+    const chainedFunction = chainer(functions);
 
-    chainer(functions)(3);
+    chainedFunction(3);
 
     expect(mockAddOne).toHaveBeenCalledWith(3);
     expect(mockDouble).toHaveBeenCalledWith(4);
@@ -31,5 +32,35 @@ describe('chainer', () => {
     ];
 
     expect(chainer(functions)(2)).toBe(36);
+  });
+
+  it('should handle an empty array of functions', () => {
+    expect(chainer([])(5)).toBe(5);
+  });
+
+  it('should handle a single function', () => {
+    const functions = [(x) => x * 3];
+
+    expect(chainer(functions)(3)).toBe(9);
+  });
+
+  it('should work with different types of functions', () => {
+    const functions = [
+      (x) => x.toString(),
+      (x) => x + '!',
+      (x) => x.toUpperCase(),
+    ];
+
+    expect(chainer(functions)(5)).toBe('5!');
+  });
+
+  it('should handle functions that return non-number values', () => {
+    const functions = [
+      (x) => x.toString(),
+      (x) => parseInt(x) + 2,
+      (x) => x * x,
+    ];
+
+    expect(chainer(functions)(2)).toBe(16);
   });
 });
