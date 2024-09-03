44b86de8e0dc0e329ab6a37ccb1b7c0798792ff9
maksim-atanasov <atanasovmaksim56@gmail.com>
2024-09-03 10:58:45 +0300

    small changes

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -2,10 +2,22 @@
 
 const { chainer } = require('./chainer');
 
+function f1(x) {
+  return x * 2;
+}
+
+function f2(x) {
+  return x + 2;
+}
+
+function f3(x) {
+  return Math.pow(x, 2);
+}
+
 describe(`'chainer' function`, () => {
-  const f1 = (x) => x * 2;
-  const f2 = (x) => x + 2;
-  const f3 = (x) => Math.pow(x, 2);
+  it('should be a function', () => {
+    expect(chainer).toBeInstanceOf(Function);
+  });
 
   it('should work correctly with many functions', () => {
     expect(chainer([f1, f2, f3])(0)).toBe(4);
