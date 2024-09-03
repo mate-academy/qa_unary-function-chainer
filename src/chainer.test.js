f6ba4ada39e20ea633f12dcfcbcf600b9e5caac0
maksim-atanasov <atanasovmaksim56@gmail.com>
2024-09-03 10:53:29 +0300

    solution

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,1 +1,17 @@
+'use strict';
+
 const { chainer } = require('./chainer');
+
+describe(`'chainer' function`, () => {
+  const f1 = (x) => x * 2;
+  const f2 = (x) => x + 2;
+  const f3 = (x) => Math.pow(x, 2);
+
+  it('should work correctly with many functions', () => {
+    expect(chainer([f1, f2, f3])(0)).toBe(4);
+  });
+
+  it('should work correctly with 1 function', () => {
+    expect(chainer([f1])(10)).toBe(20);
+  });
+});
