60e1e20a94a854d8a1552c5d5e3cdea5da5b2898
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-26 21:35:52 +0300

    chainer

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,1 +1,31 @@
+'use strict';
+
 const { chainer } = require('./chainer');
+
+describe('chainer', () => {
+  it('should correctly chain functions', () => {
+    const f1 = jest.fn((x) => x * 2);
+    const f2 = jest.fn((x) => x + 2);
+
+    const result = chainer([f1, f2])(2);
+
+    expect(result).toBe(6);
+    expect(f1).toHaveBeenCalledTimes(1);
+    expect(f2).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return value if empty array as argument', () => {
+    const result = chainer([])(2);
+
+    expect(result).toBe(2);
+  });
+
+  it('should correctly apply a single function', () => {
+    const f1 = jest.fn((x) => x * 2);
+
+    const result = chainer([f1])(2);
+
+    expect(result).toBe(4);
+    expect(f1).toHaveBeenCalledTimes(1);
+  });
+});
