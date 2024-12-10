46f42984b880f003fb625f011a494b80b20cf8f2
Galers <galosaandrew@gmail.com>
2024-12-10 19:14:46 +0200

    test: Add tests

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 describe(`chainer`, () => {
-  const { chainer } = require('./chainer');
+  const { chainer } = require("./chainer");
 
   it(`should correctly chain and execute functions with arguments`, () => {
     const f1 = jest.fn((x) => x * 2);
@@ -12,14 +12,36 @@ describe(`chainer`, () => {
     const f6 = jest.fn((x) => x - 5);
 
     const arr = [f1, f2, f3, f4, f5, f6];
+    const result = chainer(arr)(5);
 
-    chainer(arr);
+    expect(result).toEqual(-5);
+  });
 
-    expect(f1(2)).toEqual(4);
-    expect(f2(4)).toEqual(6);
-    expect(f3(1)).toEqual(1);
-    expect(f4(5)).toEqual(1);
-    expect(f5(10)).toEqual(5);
-    expect(f6(1)).toEqual(-4);
+  it("should call all functions in the array", () => {
+    const f1 = jest.fn((x) => x + 1);
+    const f2 = jest.fn((x) => x * 2);
+    const f3 = jest.fn((x) => x - 3);
+
+    const arr = [f1, f2, f3];
+
+    chainer(arr)(4);
+
+    expect(f1).toHaveBeenCalled();
+    expect(f2).toHaveBeenCalled();
+    expect(f3).toHaveBeenCalled();
+  });
+
+  it("should call functions in the correct order", () => {
+    const f1 = jest.fn((x) => x + 1);
+    const f2 = jest.fn((x) => x * 2);
+    const f3 = jest.fn((x) => x - 3);
+
+    const arr = [f1, f2, f3];
+
+    chainer(arr)(5);
+
+    expect(f1).toHaveBeenCalledWith(5);
+    expect(f2).toHaveBeenCalledWith(f1.mock.results[0].value);
+    expect(f3).toHaveBeenCalledWith(f2.mock.results[0].value);
   });
 });
