7f122fe21687a3a879f19eb49aee56ca7ba8524f
Galers <galosaandrew@gmail.com>
2024-12-10 18:54:31 +0200

    test: Add tests

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -7,23 +7,19 @@ describe(`chainer`, () => {
     const f1 = jest.fn((x) => x * 2);
     const f2 = jest.fn((x) => x + 2);
     const f3 = jest.fn((x) => Math.pow(x, 2));
+    const f4 = jest.fn((x) => x % 2);
+    const f5 = jest.fn((x) => x / 2);
+    const f6 = jest.fn((x) => x - 5);
 
-    chainer(f1, f2, f3);
+    const arr = [f1, f2, f3, f4, f5, f6];
+
+    chainer(arr);
 
     expect(f1(2)).toEqual(4);
     expect(f2(4)).toEqual(6);
     expect(f3(1)).toEqual(1);
-  });
-
-  it(`should call all functions in the chain`, () => {
-    const f1 = jest.fn((x) => x % 2);
-    const f2 = jest.fn((x) => x / 2);
-    const f3 = jest.fn((x) => x - 5);
-
-    chainer(f1, f2, f3);
-
-    expect(f1(5)).toEqual(1);
-    expect(f2(10)).toEqual(5);
-    expect(f3(1)).toEqual(-4);
+    expect(f4(5)).toEqual(1);
+    expect(f5(10)).toEqual(5);
+    expect(f6(1)).toEqual(-4);
   });
 });
