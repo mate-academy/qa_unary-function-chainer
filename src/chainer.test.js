b80ede907e575b2ce69f78cad005b3325b434fa4
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-01-24 01:06:23 +0200

    test for chainer function

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,1 +1,24 @@
+'use strict';
+
 const { chainer } = require('./chainer');
+
+describe('chainer', () => {
+  it(
+    //  prettier-ignore
+    'it should return a function that does'
+      + ' a left fold on the given functions',
+    () => {
+      const fn1 = jest.fn(x => x * 2);
+      const fn2 = jest.fn(x => x + 2);
+      const fn3 = jest.fn(x => Math.pow(x, 2));
+
+      const chained = chainer([fn1, fn2, fn3]);
+      const result = chained(0);
+
+      expect(fn1).toHaveBeenCalledWith(0);
+      expect(fn2).toHaveBeenCalledWith(0);
+      expect(fn3).toHaveBeenCalledWith(2);
+      expect(result).toBe(4);
+    }
+  );
+});
