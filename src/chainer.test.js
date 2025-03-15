a42c562b31b8d5900e4ddfb2554adc7bc01285b1
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2025-03-15 19:04:32 +0200

    refactor

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -3,21 +3,24 @@
 const { chainer } = require('./chainer');
 
 describe('chainer', () => {
+  let fn1, fn2, fn3, chainedFunction;
+
+  beforeEach(() => {
+    fn1 = jest.fn((x) => x + 1);
+    fn2 = jest.fn((x) => x * 2);
+    fn3 = jest.fn((x) => x - 3);
+    chainedFunction = chainer([fn1, fn2, fn3]);
+  });
+
   it('should be a function', () => {
     expect(chainer).toBeInstanceOf(Function);
   });
 
-  it('should return a function', () => {
-    expect(chainer()).toBeInstanceOf(Function);
+  it('should return a function when invoked', () => {
+    expect(chainer([])).toBeInstanceOf(Function);
   });
 
-  it('should call the functions in the correct order', () => {
-    const fn1 = jest.fn((x) => x + 1);
-    const fn2 = jest.fn((x) => x * 2);
-    const fn3 = jest.fn((x) => x - 3);
-
-    const chainedFunction = chainer([fn1, fn2, fn3]);
-
+  it('should call functions in the correct order', () => {
     chainedFunction(5);
 
     expect(fn1).toHaveBeenCalled();
@@ -34,28 +37,26 @@ describe('chainer', () => {
   });
 
   it('should return the correct result', () => {
-    const fn1 = jest.fn((x) => x + 1);
-    const fn2 = jest.fn((x) => x * 2);
-    const fn3 = jest.fn((x) => x - 3);
-
-    const chainedFunction = chainer([fn1, fn2, fn3]);
-
-    const result = chainedFunction(5);
-
-    expect(result).toBe(9);
+    expect(chainedFunction(5)).toBe(9);
   });
 
-  it('each function should receive the result of the previous function', () => {
-    const fn1 = jest.fn((x) => x + 1);
-    const fn2 = jest.fn((x) => x * 2);
-    const fn3 = jest.fn((x) => x - 3);
-
-    const chainedFunction = chainer([fn1, fn2, fn3]);
-
+  it('should pass the output of each function as input to the next', () => {
     chainedFunction(5);
 
     expect(fn1).toHaveBeenCalledWith(5);
     expect(fn2).toHaveBeenCalledWith(6);
     expect(fn3).toHaveBeenCalledWith(12);
+  });
+
+  it('should return the same input when given an empty function array', () => {
+    const emptyChainer = chainer([]);
+
+    expect(emptyChainer(5)).toBe(5);
+  });
+
+  it('should return correct result when only one function is provided', () => {
+    const singleChainer = chainer([fn1]);
+
+    expect(singleChainer(10)).toBe(11);
   });
 });
